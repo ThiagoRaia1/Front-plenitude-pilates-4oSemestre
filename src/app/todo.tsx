817474e602eda4bab27fbed2a18d13b65ac6c0f6
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2024-11-14 19:37:19 -0300

    Primeira tela de login

---
+++ src/app/todo.tsx
@@ -0,0 +1,9 @@
+const Todo = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">To-Do List</h1>
+        </div>
+    );
+};
+
+export default Todo
