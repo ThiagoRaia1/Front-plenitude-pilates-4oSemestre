517559fa5413b9f0950d2c9527bdece9df661dcd
Bryan Vinicius Gomes <gomesbryan754@gmail.com>
2024-11-21 21:54:49 -0300

    adição do calendario

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState } from "react";
 import { callCreate, ICreateAluno } from "./api";
+import Calendar from "../calendar/page";
 
 const Page = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -110,41 +111,11 @@ const Page = () => {
         Pesquisar aluno
       </button>
     </div>
-
+<div className="mt-8" >
+  <Calendar></Calendar>
+</div>
     <div >
-    <table className="w-full border-collapse border border-blue-500 max-w-xl mt-16 mx-auto">
-      <thead>
-        <tr className="bg-blue-500 text-white">
-          <th className="py-2 px-4 text-left">id</th>
-          <th className="py-2 px-4 text-left">Cpf</th>
-          <th className="py-2 px-4 text-left">status</th>
-          <th className="py-2 px-4 text-left">Ultima Alteracao</th>
-          <th className="py-2 px-4 text-left">Data ultima Alteracao</th>
-          <th className="py-2 px-4 text-left">numero Rua</th>
-          <th className="py-2 px-4 text-left">numero Casa</th>
-          <th className="py-2 px-4 text-left">cep</th>
-          <th className="py-2 px-4 text-left">bairro</th>
-          <th className="py-2 px-4 text-left">cidade</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="bg-white border-b border-blue-500">
-          <td className="py-2 px-4">John Doe</td>
-          <td className="py-2 px-4">25</td>
-          <td className="py-2 px-4">New York</td>
-        </tr>
-        <tr className="bg-white border-b border-blue-500">
-          <td className="py-2 px-4">Jane Smith</td>
-          <td className="py-2 px-4">30</td>
-          <td className="py-2 px-4">Los Angeles</td>
-        </tr>
-        <tr className="bg-white border-b border-blue-500">
-          <td className="py-2 px-4">Bob Johnson</td>
-          <td className="py-2 px-4">40</td>
-          <td className="py-2 px-4">Chicago</td>
-        </tr>
-      </tbody>
-    </table>
+    
      
         
   {isModalOpen && (
